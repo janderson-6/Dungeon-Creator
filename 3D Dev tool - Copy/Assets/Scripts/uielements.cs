f3f0aa057eae4dcb25b1b6125efe0e1a28687bc5
janderson-6 <andersonzelda@gmail.com>
2021-11-22 11:50:58 +0000

    Colours and materials

--- 3D Dev tool - Copy/Assets/Scripts/uielements.cs
+++ 3D Dev tool - Copy/Assets/Scripts/uielements.cs
@@ -7,14 +7,18 @@ using System;
 enum UIdata
 {
     MainMenu,
-    Drawing,
-    Materials
+    Drawing
+    
 }
 
 public class uielements : EditorWindow
 {
 
     //Variables
+    
+    Color[] ColourArray = new Color[9];
+    Material Selectedmaterial;
+    Material[] materialArray = new Material[9];
     UIdata currentState = UIdata.MainMenu;
     Tilemanager currentManager;
     Tilemanager[] allTileManagers = new Tilemanager[0];
@@ -38,7 +42,7 @@ public class uielements : EditorWindow
         uielements window = (uielements)EditorWindow.GetWindow(typeof(uielements), false, "Open Editor");
         window.Show();
     }
-        
+
     void OnEnable()
     {
         ChangeState(UIdata.MainMenu);
@@ -48,7 +52,7 @@ public class uielements : EditorWindow
         {
             redblock[i] = Color.red;
         }
-     
+
         grayblock = new Color[blocksize * blocksize];
         for (int i = 0; i < blocksize * blocksize; i++)
         {
@@ -66,6 +70,30 @@ public class uielements : EditorWindow
         }
         texture.Apply();
         Repaint();
+
+        materialArray[0] = (Material)Resources.Load("Materials/Dirt");
+        ColourArray[0] = new Color32 (62,41,26,255);
+        materialArray[1] = (Material)Resources.Load("Materials/Grass");
+        ColourArray[1] = new Color32(87,106,29,255);
+        materialArray[2] = (Material)Resources.Load("Materials/Wooden texture");
+        ColourArray[2] = new Color32(82,47,27,255);
+        materialArray[3] = (Material)Resources.Load("Materials/Bronze Sand");
+        ColourArray[3] = new Color32(139,101,58,255);
+        materialArray[4] = (Material)Resources.Load("Materials/Sandstone");
+        ColourArray[4] = new Color32(171,140,100,255);
+        materialArray[5] = (Material)Resources.Load("Materials/Dark Stone");
+        ColourArray[5] = new Color32(65,70,76,255);
+        materialArray[6] = (Material)Resources.Load("Materials/Square Slabs");
+        ColourArray[6] = new Color32(75,80,77,255);
+        materialArray[7] = (Material)Resources.Load("Materials/Rock Texture");
+        ColourArray[7] = new Color32(104,91,75,255);
+        materialArray[8] = (Material)Resources.Load("Materials/Multicolour gravel");
+        ColourArray[8] = new Color32(92,89,89,255);
+
+        Selectedmaterial = materialArray[0];
+        ChangeBrush(ColourArray[0]);
+
+
     }
 
     void OnGUI()
@@ -81,11 +109,7 @@ public class uielements : EditorWindow
                     currentManager = allTileManagers[i];
                     ChangeState(UIdata.Drawing);
                 }
-                if (GUILayout.Button("Edit " + allTileManagers[i].name + " Materials"))
-                {
-                    currentManager = allTileManagers[i];
-                    ChangeState(UIdata.Materials);
-                }
+                
             }
          if (GUILayout.Button("Draw new room"))
          {
@@ -144,21 +168,27 @@ public class uielements : EditorWindow
             texture.Apply();
             Repaint();
             GUI.DrawTexture(new Rect(0, 0, background, background), texture);
-           
+
+            for (int i = 0; i < materialArray.Length; i++)
+            {
+                GUIContent content = new GUIContent(AssetPreview.GetAssetPreview(materialArray[i]));
+                if (GUI.Button(new Rect(500, 50*i, 50, 50), content))
+                {
+                    Selectedmaterial = materialArray[i];
+                    ChangeBrush(ColourArray[i]);
+                }
             }
-        #endregion
 
-        #region Materials
-        if (currentState == UIdata.Materials)
-        {
-            //this code does nothing
-            Changematerials();
         }
-
-
-            #endregion
+        #endregion   
       }
-
+    void ChangeBrush(Color32 newColour)
+    {
+        for (int i = 0; i < blocksize * blocksize; i++)
+        {
+            redblock[i] = newColour;
+        }
+    }
 
     //Changing states
     void ChangeState(UIdata newState)
@@ -176,6 +206,10 @@ public class uielements : EditorWindow
                 {
                     if (currentManager.tilesInThisObject[x, y] != null)
                     {
+                        int index = Array.IndexOf(materialArray, currentManager.tilesInThisObject[x, y].GetComponent<Renderer>().sharedMaterial);
+                        Debug.Log("help, "+ index);
+                        ChangeBrush(ColourArray[index]);
+                        
                         texture.SetPixels((blocksize * x), blocksize * (currentManager.tilesInThisObject.GetLength(1) - (y + 1)), blocksize, blocksize, redblock);
                     }                    
                                         
@@ -185,11 +219,7 @@ public class uielements : EditorWindow
     }
 
     //Math for rounding for upper and lower bounds
-    private float Round(float input)
-    {
-        return blocksize * Mathf.Round((input / blocksize));
-
-    }
+   
     private float Floor(float input)
     {
         return blocksize * Mathf.Floor((input / blocksize));
@@ -213,6 +243,7 @@ public class uielements : EditorWindow
                 floortile.transform.position = new Vector3Int((-unityxvalue + (int)currentManager.transform.position.x), 0, (unityyvalue + (int)currentManager.transform.position.z));
                 currentManager.tilesInThisObject[unityxvalue, unityyvalue] = floortile;
                 floortile.transform.SetParent(currentManager.gameObject.transform, true);
+                Changematerials();
 
 
                 //LEFTWALLS
@@ -236,7 +267,7 @@ public class uielements : EditorWindow
 
                 //RIGHTWALLS
                 //IF ON VERY RIGHT
-                if (currentManager.tilesInThisObject.GetLength(0)-1 == unityxvalue)
+                if (currentManager.tilesInThisObject.GetLength(0) - 1 == unityxvalue)
                 {
                     Rightwall();
                 }
@@ -255,7 +286,7 @@ public class uielements : EditorWindow
 
                 //FRONTWALLS
                 //IF ON VERY FRONT
-                if (0 == unityyvalue) 
+                if (0 == unityyvalue)
                 {
                     Frontwall();
                 }
@@ -274,7 +305,7 @@ public class uielements : EditorWindow
 
                 //BACKWALLS
                 //IF ON VERY BACK
-                if (currentManager.tilesInThisObject.GetLength(1)-1 == unityyvalue)
+                if (currentManager.tilesInThisObject.GetLength(1) - 1 == unityyvalue)
                 {
                     Backwall();
                 }
@@ -289,17 +320,19 @@ public class uielements : EditorWindow
                 {
                     Backwall();
                 }
-                
+
 
             }
+            else Changematerials();
         }
+
     }
 
     void Changematerials()
     {//this function does nothing
         if (currentManager.tilesInThisObject[unityxvalue, unityyvalue] != null)
         {//this calls nothing
-            currentManager.tilesInThisObject[unityxvalue, unityyvalue].GetComponent<Renderer>().sharedMaterials[0] = (Material)Resources.Load("Materials/Dark Stone");
+            currentManager.tilesInThisObject[unityxvalue, unityyvalue].GetComponent<Renderer>().material = Selectedmaterial;
             
         }
     }
@@ -371,7 +404,9 @@ public class uielements : EditorWindow
     }
 
 }
+
 #endregion
+
 
 
     
